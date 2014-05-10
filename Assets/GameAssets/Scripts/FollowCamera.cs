697194864c2331ed0f6e793dc5877b5a8b2180d6
Louis Heitfeld <louis.heitfeld@gmail.com>
2014-05-10 11:51:02 -0600

    init commit

---
+++ Assets/GameAssets/Scripts/FollowCamera.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCamera : MonoBehaviour {
+
+    public GameObject player;
+    public Vector3 speed = new Vector3(10f, 10f, 10f);
+    Transform myTransform;
+    Transform playerTransform;
+    bool bAtMaxDistance = false;
+
+
+    void Update ()
+    {
+        SetCameraPosition();
+    }
+
+    void OnEnable()
+    {
+        playerTransform = player.transform;
+        myTransform = transform;
+    }
+
+    void SetCameraPosition()
+    {
+        int player = Mathf.FloorToInt(playerTransform.localPosition.x);
+        int camera = Mathf.FloorToInt(myTransform.localPosition.x);
+
+        this.transform.localPosition = new Vector3(playerTransform.localPosition.x, myTransform.localPosition.y, myTransform.localPosition.z);
+    }
+}
---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gun : MonoBehaviour
+{
+	public Rigidbody2D rocket;				// Prefab of the rocket.
+	public float speed = 20f;				// The speed the rocket will fire at.
+
+
+	private PlayerControl playerCtrl;		// Reference to the PlayerControl script.
+	private Animator anim;					// Reference to the Animator component.
+
+
+	void Awake()
+	{
+		// Setting up the references.
+		anim = transform.root.gameObject.GetComponent<Animator>();
+		playerCtrl = transform.root.GetComponent<PlayerControl>();
+	}
+
+
+	void Update ()
+	{
+		// If the fire button is pressed...
+		if(Input.GetButtonDown("Fire1"))
+		{
+			// ... set the animator Shoot trigger parameter and play the audioclip.
+			anim.SetTrigger("Shoot");
+			audio.Play();
+
+            //Input.mousePosition;
+            int halfScreenWidth = Screen.width / 2;
+            int halfScreenHeight = Screen.height / 2;
+
+            int mousePosX = Mathf.FloorToInt(Input.mousePosition.x);
+            int mousePosY = Mathf.FloorToInt(Input.mousePosition.y);
+
+            if (mousePosX < halfScreenWidth)
+            {
+                // ... instantiate the rocket facing right and set it's velocity to the right. 
+                Rigidbody2D bulletInstance = Instantiate(rocket, transform.position, Quaternion.Euler(new Vector3(0, 0, 180f))) as Rigidbody2D;
+                bulletInstance.velocity = new Vector2(-speed, 0);
+            }
+            else
+            {
+                // Otherwise instantiate the rocket facing left and set it's velocity to the left.
+                Rigidbody2D bulletInstance = Instantiate(rocket, transform.position, Quaternion.Euler(new Vector3(0, 0, 0))) as Rigidbody2D;
+                bulletInstance.velocity = new Vector2(speed, 0);
+            }
+
+            //// If the player is facing right...
+            //if(playerCtrl.facingRight)
+            //{
+            //    // ... instantiate the rocket facing right and set it's velocity to the right. 
+            //    Rigidbody2D bulletInstance = Instantiate(rocket, transform.position, Quaternion.Euler(new Vector3(0,0,0))) as Rigidbody2D;
+            //    bulletInstance.velocity = new Vector2(speed, 0);
+            //}
+            //else
+            //{
+            //    // Otherwise instantiate the rocket facing left and set it's velocity to the left.
+            //    Rigidbody2D bulletInstance = Instantiate(rocket, transform.position, Quaternion.Euler(new Vector3(0,0,180f))) as Rigidbody2D;
+            //    bulletInstance.velocity = new Vector2(-speed, 0);
+            //}
+		}
+	}
+}
